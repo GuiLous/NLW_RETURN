5d72882b05939bd9292131972ede31663da4756c
Guilherme Lourenço da Silva <guilhermesilva@acad.ifma.edu.br>
2022-06-17 15:22:40 -0300

    done

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -75,7 +75,7 @@ export function WidgetForm() {
       )}
 
       <footer className="text-xs text-neutral-400">
-        Feito ❤️ com pelo{" "}
+        Feito ❤️ com por Guilherme Lourenco
         <a
           className="underline underline-offset-2"
           href="https://portfolio-jrxr.vercel.app/"
